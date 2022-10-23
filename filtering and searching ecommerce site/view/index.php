8cedd0b0743c10b79a00dd1fe94892cd82cbb0b6
unknown <dvlpt.rijon@gmail.com>
2022-10-23 17:43:06 +0600

    update

--- filtering and searching ecommerce site/view/index.php
+++ filtering and searching ecommerce site/view/index.php
@@ -7,6 +7,8 @@
     <script type="text/javascript" src="../view/js/search.js"></script>
     <script src="https://kit.fontawesome.com/3dfa0d7dbe.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../view/css/nav.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <title></title>
 </head>
 <body>
@@ -14,8 +16,6 @@
    <div class="heading">
       
            <h1 class="logoName">Nemesis</h1>
-           <input class="search" id="search" type="text" placeholder="Search here" onkeyup="search()">
-           <input class="login" type="button" value="login">
 
    </div>
     <!--Heading Section end-->
@@ -23,6 +23,7 @@
     <div class="task">
         <?php include 'task.html';?>
     </div>
+    <br><br>
     <!--Menu bar section close-->
     <!--Footer Section-->
     <div class="End">
